efdcd94fb889f80808bdb260dc7626e5914594fa
kirillkaluznyj <kirill.webmail@yandex.com>
2025-01-02 21:19:23 +0200

    version: 0.5.1

--- lib/src/image_crop.dart
+++ lib/src/image_crop.dart
@@ -10,7 +10,7 @@ class ImageOptions {
   });
 
   @override
-  int get hashCode => hashValues(width, height);
+  int get hashCode => Object.hash(width, height);
 
   @override
   bool operator ==(other) =>
@@ -21,18 +21,15 @@ class ImageOptions {
 }
 
 class ImageCrop {
-  static const _channel =
-      const MethodChannel('plugins.lykhonis.com/image_crop');
+  static const _channel = const MethodChannel('plugins.lykhonis.com/image_crop');
 
-  static Future<bool> requestPermissions() => _channel
-      .invokeMethod('requestPermissions')
-      .then<bool>((result) => result);
+  static Future<bool> requestPermissions() =>
+      _channel.invokeMethod('requestPermissions').then<bool>((result) => result);
 
   static Future<ImageOptions> getImageOptions({
     required File file,
   }) async {
-    final result =
-        await _channel.invokeMethod('getImageOptions', {'path': file.path});
+    final result = await _channel.invokeMethod('getImageOptions', {'path': file.path});
 
     return ImageOptions(
       width: result['width'],
@@ -61,8 +58,7 @@ class ImageCrop {
     int? preferredHeight,
   }) async {
     assert(() {
-      if (preferredSize == null &&
-          (preferredWidth == null || preferredHeight == null)) {
+      if (preferredSize == null && (preferredWidth == null || preferredHeight == null)) {
         throw ArgumentError(
             'Preferred size or both width and height of a resampled image must be specified.');
       }
